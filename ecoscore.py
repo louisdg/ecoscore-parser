205f2a5d924590abba786e858ca63705dfba2b9f
Author Louis De Gaudenzi <louis.de-gaudenzi@etu.univ-grenoble-alpes.fr>
2020-03-16 19:10:06 +0100

    added function ShowHelp

--- ecoscore.py
+++ ecoscore.py
@@ -4,3 +4,18 @@
 import os, sys
 
 appDirectory = ""
+
+def ShowHelp(detailed):
+    if(detailed):
+        print("\necoscore.py")
+        print("\nSynopsis")
+        print("\n\tpython3 ecoscore.py [OPTION]... APP_DIRECTORY")
+        print("\nDescription")
+        print("\n\tAnalyses and gives a score to the app located in the directory `APP_DIRECTORY`.")
+        print("\n\tMandatory arguments to long options are mandatory for short options too.")
+        print("\n\t`-v`, `--verbose`", "\t\tprint a message for each file analysed", sep="\n")
+        print("\n\t`--help`", "\t\tdisplay help and exit", sep="\n")
+    else:
+        print("\nUsage")
+        print("\n\tpython3 ecoscore.py [OPTION]... APP_DIRECTORY")
+        print("\n\tUse option --help for detailed help.")
